ad706b1d2f1c73436fea30e2c1d7b2152590b032
zhankaam <zhankaam@gmail.com>
2021-03-18 22:43:42 +0300

    portfolio!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./header/Header";
+import {Main} from "./main/Main";
+import {Skills} from "./skills/Skills";
+import {Content} from "./content/Content";
+import {Section} from "./part/Section";
+import {Contact} from "./contact/Contact";
+import {Footer} from "./footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <Main/>
+     <Skills/>
+     <Content/>
+     <Section/>
+     <Contact/>
+     <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App
