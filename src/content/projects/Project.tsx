5fbfb7f5178396c1c388e2802ec5d4b79bc8e541
zhankaam <zhankaam@gmail.com>
2021-03-25 23:54:34 +0300

    portfolio!

--- src/content/projects/Project.tsx
+++ src/content/projects/Project.tsx
@@ -11,7 +11,7 @@ export const Project = (props: PropsType) => {
     return (
         <div className={style.project}>
             <div className={style.photo} style={props.style}>
-                <a className={style.viewButton}>more details</a>
+                <a href={""} className={style.viewButton}>more details</a>
             </div>
             <div className={style.projectInfo}>
                 <h3 className={style.projectName}>{props.title}</h3>
--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -6,7 +6,23 @@ export function Footer() {
     return (
         <div className={s.footer}>
             <div className={`${styleContainer.container} ${s.footerContainer}`}>
-                <h3 className={s.title}>Zhanat Lepesbayeva</h3>
+                <div className={s.infoBox}>
+                    <div className={s.form}>
+                        <div className={s.icon}>icon</div>
+                        <h5 className={s.mainText}>Address Line</h5>
+                        <p className={s.paragraph}>Minsk city,Belarus</p>
+                    </div>
+                    <div className={s.form}>
+                        <div className={s.icon}>icon</div>
+                        <h5 className={s.mainText}>Mail</h5>
+                        <p className={s.paragraph}>zhankaam@gmail.com</p>
+                    </div>
+                    <div className={s.form}>
+                        <div className={s.icon}>icon</div>
+                        <h5 className={s.mainText}>Phone</h5>
+                        <p className={s.paragraph}>phone: +375-396-34-14</p>
+                    </div>
+                </div>
                 <div className={s.socialMenu}>
                     <div className={s.icon}></div>
                     <div className={s.icon}></div>
