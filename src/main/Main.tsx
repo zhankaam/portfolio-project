49780e53574c5c5bc5bfa0891a2449c8d4f05b74
zhankaam <zhankaam@gmail.com>
2021-05-22 00:51:26 +0300

    portfolio!

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import s from './Main.module.scss';
-import styleContainer from '../common/styles/Container.module.scss'
+import styleContainer from '../common/styles/Container.module.scss';
+import Particles from 'react-particles-js';
+// @ts-ignore
+import Fade from 'react-reveal/Fade';
+
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        }
+    }
+}
 
 export function Main() {
     return (
         <div className={s.mainBlock}>
-            <div className={styleContainer.container}>
-                <div className={s.container}>
-                    <h5 className={s.greeting}>hello,my name is</h5>
-                    <h1 className={s.fullName}>Zhanat Lepesbayeva</h1>
-                    <span className={s.body}>I'm a Freelance Front-end Developer based in Minsk,BY</span>
-                </div>
-                <div className={s.photo}>
+            <Particles className={s.particles} params={particlesOptions}/>
 
-                </div>
+            <div className={styleContainer.container}>
+                <Fade left>
+                    <div className={s.container}>
+                        <h5 className={s.greeting}>hello,my name is</h5>
+                        <h1 className={s.fullName}>Zhanat Lepesbayeva</h1>
+                        <span className={s.body}>I'm a Freelance Front-end Developer based in Minsk,BY</span>
+                    </div>
+                </Fade>
+                <Fade right>
+                    <div className={s.photo}>
+                        <div className={s.image}></div>
+                    </div>
+                </Fade>
             </div>
         </div>
     );
