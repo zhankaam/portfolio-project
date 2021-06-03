2a151b9ee5e1a66bf783747cfbaf694ccd7db80d
zhankaam <zhankaam@gmail.com>
2021-06-03 18:11:25 +0300

    portfolio!

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -5,6 +5,8 @@ import styleContainer from '../common/styles/Container.module.scss'
 import {Title} from "../common/components/title/Title";
 import socialImage from "../../src/common/styles/image/photo-1603458319957-8df73cb52511.jpg"
 import todoListApp from "../../src/common/styles/image/photo-1558025137-0b406e9cc169.jpg"
+import teamApp from "../../src/common/styles/image/photo-1511376777868-611b54f68947.jpg"
+import calcApp from "../../src/common/styles/image/photo-1568502702280-348cf12c6d2c.jpg"
 
 export const Content = () => {
     const socialNetwork = {
@@ -13,6 +15,14 @@ export const Content = () => {
 
     const todoApp = {
         backgroundImage: `url(${todoListApp})`
+    }
+
+    const teamProject = {
+        backgroundImage: `url(${teamApp})`
+    }
+
+    const calcProject = {
+        backgroundImage: `url(${calcApp})`
     }
 
     return (
@@ -27,6 +37,12 @@ export const Content = () => {
                     <Project style={todoApp} title={"to do list"}
                              href={"https://github.com/zhankaam/todolist-tsx"}
                              text={"One of the most important reasons for keeping a to-do list is the organization. Organizing your tasks with a list can make everything much more manageable and make you feel grounded."}/>
+                    <Project style={teamProject} title={"cards project with team"}
+                             href={"https://github.com/sergeykurilov/friday"}
+                             text={"This project is being developed now by me and a team of talented creators. It will help to learn new things with the usage of cards, where, for instance, on the first side can be a word and on the other side -- a definition."}/>
+                    <Project style={calcProject} title={"сurrency сonverter"}
+                             href={"https://github.com/zhankaam/wednesday-nativka"}
+                             text={"Conversion is the knowledge of the real value of one currency to be converted into another country's currency so that you can convert between the two currencies."}/>
                 </div>
             </div>
         </div>
